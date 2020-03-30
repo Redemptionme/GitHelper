3744d5cacbb67c326a73eb6001c3fa85e703d89f
hanlihe <646167331@qq.com>
2020-03-30 18:18:31 +0800

    1

--- GitHelper/main.cpp
+++ GitHelper/main.cpp
@@ -1,8 +1,15 @@
 #include "GitHelper.h"
 #include <QtWidgets/QApplication>
+//#include "Python.h"
 
 int main(int argc, char *argv[])
 {
+   /* Py_Initialize();
+    PyRun_SimpleString("print 'hello'");
+
+    Py_Finalize();    */
+
+
     QApplication a(argc, argv);
     GitHelper w;
     w.show();
